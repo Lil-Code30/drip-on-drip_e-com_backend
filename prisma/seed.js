e3971808f32cf1d30abfd96e370c2f553a57c713
Ismael Loko <lokoismael30@gmail.com>
2025-07-13 11:52:02 -0400

    tryingto add user data using seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -9,56 +9,62 @@ When you want to use prisma migrate dev or prisma migrate reset without seeding,
 const prisma = new PrismaClient();
 
 async function main() {
-  for (const product of allProducts) {
-    // Ensure category exists or create it
-    const category = await prisma.Category.upsert({
-      where: { name: product.category },
-      update: {},
-      create: { name: product.category },
-    });
-    // Ensure brand exists or create it
-    const brand = await prisma.Brand.upsert({
-      where: { name: product.brand },
-      update: {},
-      create: { name: product.brand },
-    });
-    // Generate a UUID for product id
-    const productId = uuid4();
-    // Generate SKU if not present
-    const sku =
-      product.sku ||
-      generateSKU({
-        name: product.title || product.name,
-        category: product.category,
-      });
-    // Upsert product
-    const createdProduct = await prisma.Product.upsert({
-      where: { id: productId },
-      update: {},
-      create: {
-        id: productId,
-        name: product.title || product.name,
-        images: product.images,
-        description: product.description,
-        price: product.price,
-        categoryId: category.id,
-        discountPercentage: Math.round(product.discountPercentage || 0),
-        rating: product.rating || 0,
-        stock: product.stock || 0,
-        tags: product.tags || [],
-        brandId: brand.id,
-        sku,
-        weight: product.weight || null,
-        dimensions: product.dimensions ? product.dimensions : null,
-        warrantyInformation: product.warrantyInformation || null,
-        shippingInformation: product.shippingInformation || null,
-        availabilityStatus: product.availabilityStatus || "in_stock",
-        returnPolicy: product.returnPolicy || null,
-        minimumOrderQuantity: product.minimumOrderQuantity || 1,
-      },
-    });
-    console.log(`Product created: ${createdProduct.name}`);
-  }
+  // for (const product of allProducts) {
+  //   // Ensure category exists or create it
+  //   const category = await prisma.Category.upsert({
+  //     where: { name: product.category },
+  //     update: {},
+  //     create: { name: product.category },
+  //   });
+  //   // Ensure brand exists or create it
+  //   const brand = await prisma.Brand.upsert({
+  //     where: { name: product.brand },
+  //     update: {},
+  //     create: { name: product.brand },
+  //   });
+  //   // Generate a UUID for product id
+  //   const productId = uuid4();
+  //   // Generate SKU if not present
+  //   const sku =
+  //     product.sku ||
+  //     generateSKU({
+  //       name: product.title || product.name,
+  //       category: product.category,
+  //     });
+  //   // Upsert product
+  //   const createdProduct = await prisma.Product.upsert({
+  //     where: { id: productId },
+  //     update: {},
+  //     create: {
+  //       id: productId,
+  //       name: product.title || product.name,
+  //       images: product.images,
+  //       description: product.description,
+  //       price: product.price,
+  //       categoryId: category.id,
+  //       discountPercentage: Math.round(product.discountPercentage || 0),
+  //       rating: product.rating || 0,
+  //       stock: product.stock || 0,
+  //       tags: product.tags || [],
+  //       brandId: brand.id,
+  //       sku,
+  //       weight: product.weight || null,
+  //       dimensions: product.dimensions ? product.dimensions : null,
+  //       warrantyInformation: product.warrantyInformation || null,
+  //       shippingInformation: product.shippingInformation || null,
+  //       availabilityStatus: product.availabilityStatus || "in_stock",
+  //       returnPolicy: product.returnPolicy || null,
+  //       minimumOrderQuantity: product.minimumOrderQuantity || 1,
+  //     },
+  //   });
+  //   console.log(`Product created: ${createdProduct.name}`);
+  // }
+
+  const createdUser = await prisma.Category.upsert({
+    where: { email: product.category },
+    update: {},
+    create: { name: product.category },
+  });
 }
 
 main()
---
+++ routes/cart.routes.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const cartRoute = express.Router();
+
+export default cartRoute;
