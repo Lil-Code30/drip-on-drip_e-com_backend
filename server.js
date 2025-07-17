59db04517ba1030985072eb06fc5700a8e01ee97
Ismael Loko <lokoismael30@gmail.com>
2025-07-17 12:01:26 -0400

    added allowed origins on the server

--- server.js
+++ server.js
@@ -12,7 +12,24 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(cors());
+const allowedOrigins = [
+  "https://drip-on-drip-v1.vercel.app",
+  "http://localhost:5173",
+];
+const corsOption = {
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true, // If you need to send cookies
+  optionsSuccessStatus: 200, // Some legacy browsers choke on 204
+};
+app.use(cors(corsOption));
 //  Use cookie-parser middleware
 app.use(cookieParser());
 app.use(express.json());
