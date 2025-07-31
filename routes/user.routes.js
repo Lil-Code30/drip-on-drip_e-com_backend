ed15526a4d6d438d34e250a51dcea91530928ea1
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-07-31 16:25:20 -0400

    user can add a new address

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,13 +3,15 @@ import { authMiddleware } from "../middlewares/auth.middlewares.js";
 import {
   getUserProfile,
   updateUserProfile,
-  changeUserPassword
+  changeUserPassword,
+  changeUserAddress,
 } from "../controllers/user.controller.js";
 
 const userRoute = express.Router();
 
 userRoute.get("/profile", authMiddleware, getUserProfile);
 userRoute.put("/profile", authMiddleware, updateUserProfile);
-userRoute.put("change-password", authMiddleware, changeUserPassword);
+userRoute.put("/change-password", authMiddleware, changeUserPassword);
+userRoute.post("/add-address", authMiddleware, changeUserAddress);
 
 export default userRoute;
