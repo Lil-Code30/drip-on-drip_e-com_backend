a9ff84ee45f6678aee526fa73144e57104d61427
Ismael Loko <lokoismael30@gmail.com>
2025-08-02 10:18:01 -0400

    debug payment creation controller for correct connection to the front

--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
 import { createOrderAndPayment } from "../controllers/payment.controllers.js";
+import { authMiddleware } from "../middlewares/auth.middlewares.js";
 
 const PaymentRoute = express.Router();
 
-PaymentRoute.post("/create-payment-intent", createOrderAndPayment);
+PaymentRoute.post(
+  "/create-payment-intent",
+
+  createOrderAndPayment
+);
 
 export default PaymentRoute;
