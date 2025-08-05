f6c1b6cb7a1689ad512ec1dbe8ebb7b46a339fed
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2025-08-05 13:43:55 -0400

    migrate db to prisma workstation

--- utils/dbConnection.js
+++ utils/dbConnection.js
@@ -1,16 +1,7 @@
-import { PrismaClient } from "@prisma/client";
-
-const Prisma = new PrismaClient();
-export default Prisma;
-
 // PGADMIN_DB_URL=postgresql://postgres:1234@localhost:5432/dripondrip
 
-// import { PrismaPg } from "@prisma/adapter-pg";
-// import { PrismaClient } from "../generated/prisma/index.js";
+import { PrismaClient } from "../generated/prisma/index.js";
 
-// const connectionString = `${process.env.DATABASE_URL}`;
+const Prisma = new PrismaClient();
 
-// const adapter = new PrismaPg({ connectionString });
-// const Prisma = new PrismaClient({ adapter });
-
-// export default Prisma;
+export default Prisma;
